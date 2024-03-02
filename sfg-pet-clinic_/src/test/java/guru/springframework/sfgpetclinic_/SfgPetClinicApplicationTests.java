6013990eb18ff40ed0f70ff9f19f537a7ece3f98
Alisweid168 <ali.sweidan003@gmail.com>
2024-03-02 16:13:25 +0200

    Initial Project Add

---
+++ sfg-pet-clinic_/src/test/java/guru/springframework/sfgpetclinic_/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
